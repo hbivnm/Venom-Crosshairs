1672c08d449744ac9804cb98d4fdfc8a97207ce2
HybridVenom <alexander.jonsson987@gmail.com>
2022-04-05 12:45:47 +0200

    Add project files.

---
+++ TF2WeaponSpecificCrosshairs/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace TF2WeaponSpecificCrosshairs
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormMain";
+            this.Text = "TF2 Weapon Specific Crosshairs";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
