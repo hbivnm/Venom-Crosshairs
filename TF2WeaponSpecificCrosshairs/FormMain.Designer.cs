69c9a2327c3c766cdcafadae7a3e3c03161b70ff
HybridVenom <alexander.jonsson987@gmail.com>
2022-05-16 15:30:45 +0200

    Initial code commit, includes: - Sanity check function - Initialization on startup - Debugger - Class, Weapon and Crosshair comboboxes - Reload button (for crosshair list) - Reload crosshair list function - Image preview of selected crosshair - All TF2 classes and their respective weapons

---
+++ TF2WeaponSpecificCrosshairs/FormMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TF2WeaponSpecificCrosshairs
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.textBoxTF2Path = new System.Windows.Forms.TextBox();
+            this.lblTF2Path = new System.Windows.Forms.Label();
+            this.textBoxDebugger = new System.Windows.Forms.TextBox();
+            this.cbClass = new System.Windows.Forms.ComboBox();
+            this.cbWeapon = new System.Windows.Forms.ComboBox();
+            this.cbCrosshair = new System.Windows.Forms.ComboBox();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.lblWeapon = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBoxCrosshair = new System.Windows.Forms.PictureBox();
+            this.btnReload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCrosshair)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTF2Path
+            // 
+            this.textBoxTF2Path.Location = new System.Drawing.Point(15, 24);
+            this.textBoxTF2Path.Name = "textBoxTF2Path";
+            this.textBoxTF2Path.Size = new System.Drawing.Size(364, 20);
+            this.textBoxTF2Path.TabIndex = 3;
+            this.textBoxTF2Path.Text = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Team Fortress 2";
+            // 
+            // lblTF2Path
+            // 
+            this.lblTF2Path.AutoSize = true;
+            this.lblTF2Path.Location = new System.Drawing.Point(12, 9);
+            this.lblTF2Path.Name = "lblTF2Path";
+            this.lblTF2Path.Size = new System.Drawing.Size(54, 13);
+            this.lblTF2Path.TabIndex = 2;
+            this.lblTF2Path.Text = "TF2 Path:";
+            // 
+            // textBoxDebugger
+            // 
+            this.textBoxDebugger.BackColor = System.Drawing.Color.Black;
+            this.textBoxDebugger.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxDebugger.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDebugger.ForeColor = System.Drawing.SystemColors.Control;
+            this.textBoxDebugger.Location = new System.Drawing.Point(12, 385);
+            this.textBoxDebugger.Multiline = true;
+            this.textBoxDebugger.Name = "textBoxDebugger";
+            this.textBoxDebugger.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDebugger.Size = new System.Drawing.Size(837, 154);
+            this.textBoxDebugger.TabIndex = 4;
+            // 
+            // cbClass
+            // 
+            this.cbClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbClass.FormattingEnabled = true;
+            this.cbClass.Location = new System.Drawing.Point(71, 50);
+            this.cbClass.Name = "cbClass";
+            this.cbClass.Size = new System.Drawing.Size(154, 21);
+            this.cbClass.TabIndex = 5;
+            // 
+            // cbWeapon
+            // 
+            this.cbWeapon.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbWeapon.DropDownWidth = 420;
+            this.cbWeapon.FormattingEnabled = true;
+            this.cbWeapon.Location = new System.Drawing.Point(71, 77);
+            this.cbWeapon.Name = "cbWeapon";
+            this.cbWeapon.Size = new System.Drawing.Size(154, 21);
+            this.cbWeapon.TabIndex = 6;
+            // 
+            // cbCrosshair
+            // 
+            this.cbCrosshair.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCrosshair.DropDownWidth = 140;
+            this.cbCrosshair.FormattingEnabled = true;
+            this.cbCrosshair.Location = new System.Drawing.Point(71, 104);
+            this.cbCrosshair.Name = "cbCrosshair";
+            this.cbCrosshair.Size = new System.Drawing.Size(154, 21);
+            this.cbCrosshair.TabIndex = 7;
+            // 
+            // lblClass
+            // 
+            this.lblClass.AutoSize = true;
+            this.lblClass.Location = new System.Drawing.Point(30, 53);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(35, 13);
+            this.lblClass.TabIndex = 8;
+            this.lblClass.Text = "Class:";
+            // 
+            // lblWeapon
+            // 
+            this.lblWeapon.AutoSize = true;
+            this.lblWeapon.Location = new System.Drawing.Point(14, 80);
+            this.lblWeapon.Name = "lblWeapon";
+            this.lblWeapon.Size = new System.Drawing.Size(51, 13);
+            this.lblWeapon.TabIndex = 9;
+            this.lblWeapon.Text = "Weapon:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Crosshair:";
+            // 
+            // pictureBoxCrosshair
+            // 
+            this.pictureBoxCrosshair.BackgroundImage = global::TF2WeaponSpecificCrosshairs.Properties.Resources.cp_badlands;
+            this.pictureBoxCrosshair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBoxCrosshair.Image = global::TF2WeaponSpecificCrosshairs.Properties.Resources.TF2WSC;
+            this.pictureBoxCrosshair.Location = new System.Drawing.Point(231, 50);
+            this.pictureBoxCrosshair.Name = "pictureBoxCrosshair";
+            this.pictureBoxCrosshair.Size = new System.Drawing.Size(75, 75);
+            this.pictureBoxCrosshair.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxCrosshair.TabIndex = 11;
+            this.pictureBoxCrosshair.TabStop = false;
+            // 
+            // btnReload
+            // 
+            this.btnReload.Image = global::TF2WeaponSpecificCrosshairs.Properties.Resources.arrow_circle_double_135;
+            this.btnReload.Location = new System.Drawing.Point(820, 12);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(32, 32);
+            this.btnReload.TabIndex = 0;
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 551);
+            this.Controls.Add(this.pictureBoxCrosshair);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblWeapon);
+            this.Controls.Add(this.lblClass);
+            this.Controls.Add(this.cbCrosshair);
+            this.Controls.Add(this.cbWeapon);
+            this.Controls.Add(this.cbClass);
+            this.Controls.Add(this.textBoxDebugger);
+            this.Controls.Add(this.textBoxTF2Path);
+            this.Controls.Add(this.lblTF2Path);
+            this.Controls.Add(this.btnReload);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "TF2 Weapon Specific Crosshairs";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCrosshair)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.TextBox textBoxTF2Path;
+        private System.Windows.Forms.Label lblTF2Path;
+        private System.Windows.Forms.TextBox textBoxDebugger;
+        private System.Windows.Forms.ComboBox cbClass;
+        private System.Windows.Forms.ComboBox cbWeapon;
+        private System.Windows.Forms.ComboBox cbCrosshair;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.Label lblWeapon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBoxCrosshair;
+    }
+}
+
---
+++ TF2WeaponSpecificCrosshairs/FormMain.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace TF2WeaponSpecificCrosshairs
+{
+    public partial class FormMain : Form
+    {
+        private string PATH_TF2WSC = Directory.GetCurrentDirectory();
+        private string PATH_TF2WSC_RESOURCES = Directory.GetCurrentDirectory() + @"\resources\";
+        private string PATH_TF2WSC_RESOURCES_MATERIALS = Directory.GetCurrentDirectory() + @"\resources\materials\";
+        private string PATH_TF2WSC_RESOURCES_PREVIEWS = Directory.GetCurrentDirectory() + @"\resources\previews\";
+        private string PATH_TF2WSC_RESOURCES_SCRIPTS = Directory.GetCurrentDirectory() + @"\resources\scripts\";
+
+        private string FILEPATH_GENERATEPREVIEWSBAT = Directory.GetCurrentDirectory() + @"\resources\previews\generatepreviews.bat";
+
+        private bool hasInitialized = false;
+
+        private readonly string[] tf2Classes = { "Scout", "Soldier", "Pyro", "Demoman", "Heavy", "Engineer", "Medic", "Sniper", "Spy" };
+
+        private readonly string[] tf2ScoutWeapons = { "Scattergun, Back Scatter, Force-A-Nature", "Baby Face's Blaster", "Shortstop", "Soda Popper", "Pistol and all reskins (Scout)", "Bonk! Atomic Punch, Crit-a-Cola", "Flying Guillotine", "Mad Milk", "Pretty Boy's Pocket Pistol, Winger", "Bat and all reskins, Atomizer, Boston Basher, Candy Cane, Fan O'War, Sun-on-a-Stick", "Holy Mackerel", "Sandman", "Wrap Assassin" };
+        private readonly string[] tf2SoldierWeapons = { "Rocket Launcher, Black Box, Original, Liberty Launcher, Beggar's Bazooka", "Air Strike", "Cow Mangler 5000", "Direct Hit", "Shotgun, Reserve Shooter, Panic Attack", "Buff Banner, Battalion's Backup, Concheror", "Righteous Bison", "Shovel and all reskins, Equalizer, Pain Train, Disciplinary Action, Market Gardener, Escape Plan", "Half-Zatoichi" };
+        private readonly string[] tf2PyroWeapons = { "Flame Thrower and all reskins, Backburner, Degreaser, Phlogistinator", "Dragon's Fury", "Shotgun, Reserve Shooter, Panic Attack", "Flare Gun, Detonator, Scorch Shot", "Gas Passer", "Manmelter", "Thermal Thruster", "Fire Axe and all reskins, Lollichop, Axtinguisher, Homewrecker, Powerjack, Back Scratcher, Sharpened Volcano Fragment, Third Degree, Neon Annihilator", "Hot Hand" };
+        private readonly string[] tf2DemomanWeapons = { "Grenade Launcher, Loch-n-Load, Iron Bomber", "Loose Cannon", "Stickybomb Launcher, Scottish Resistance, Sticky Jumper, Quickiebomb Launcher", "Bottle and all reskins", "Eyelander, Scotsman's Skullcutter, Claidheamh Mòr, Persian Persuader, Pain Train", "Half-Zatoichi", "Ullapool Caber" };
+        private readonly string[] tf2HeavyWeapons = { "Minigun, Natascha, Brass Beast, Tomislav, Huo-Long Heater", "Shotgun, Family Business, Panic Attack", "Sandvich, Dalokohs Bar, Fishcake, Buffalo Steak Sandvich, Second Banana", "Fists and all reskins, Killing Gloves of Boxing, Gloves of Running Urgently, Warrior's Spirit, Fists of Steel, Eviction Notice, Holiday Punch" };
+        private readonly string[] tf2EngineerWeapons = { "Shotgun, Widowmaker, Panic Attack", "Frontier Justice", "Pomson 6000", "Rescue Ranger", "Pistol and all reskins (Engineer)", "Short Circuit", "Wrangler, Giger Counter", "Wrench, Southern Hospitality, Jag, Eureka Effect", "Gunslinger", "Construction PDA", "Destruction PDA", "While placing a building" };
+        private readonly string[] tf2MedicWeapons = { "Syringe Gun, Blutsauger, Overdose", "Crusader's Crossbow", "Medi Gun, Kritzkrieg, Quick-Fix, Vaccinator", "Bonesaw and all reskins, Ubersaw, Vita-Saw, Amputator, Solemn Vow" };
+        private readonly string[] tf2SniperWeapons = { "Sniper Rifle, Sydney Sleeper, Bazaar Bargain, Machina", "Classic", "Hitman's Heatmaker", "Huntsman, Fortified Compound", "SMG", "Cleaner's Carbine", "Jarate", "Kukri and all reskins, Tribalman's Shiv, Bushwacka, Shahanshah" };
+        private readonly string[] tf2SpyWeapons = { "Revolver, Ambassador, L'Etranger, Enforcer, Diamondback", "Sapper, Red-Tape Recorder", "Knife and all reskins, Your Eternal Reward, Conniver's Kunai, Big Earner, Spy-cicle", "Disguise kit" };
+
+
+        public FormMain()
+        {
+            InitializeComponent();
+            initTF2WSC();
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            if (performSanityCheck())
+                new Thread(generateCrosshairs).Start();
+        }
+
+        // Functions
+        private void initTF2WSC()
+        {
+            // Prevent initTF2WSC from running more than once
+            if (!hasInitialized)
+            {
+                // Classes
+                foreach (var tf2Class in tf2Classes)
+                    cbClass.Items.Add(tf2Class);
+                cbClass.SelectedIndexChanged += new EventHandler(onCBClassChangeEvent);
+
+                // Crosshairs
+                cbCrosshair.Items.Clear();
+                foreach (var crosshair in Directory.GetFiles(PATH_TF2WSC_RESOURCES_PREVIEWS, "*.png"))
+                {
+                    string crosshairName = Path.GetFileNameWithoutExtension(crosshair);
+                    cbCrosshair.Items.Add(crosshairName);
+                }
+                cbCrosshair.SelectedIndexChanged += new EventHandler(onCBCrosshairChangeEvent);
+            }
+        }
+
+        private void writeLineToDebugger(string text)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                textBoxDebugger.Text += text + Environment.NewLine;
+
+                // Scroll to bottom
+                textBoxDebugger.SelectionStart = textBoxDebugger.Text.Length;
+                textBoxDebugger.ScrollToCaret();
+            }));
+        }
+
+        private void writeToDebugger(string text)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                textBoxDebugger.Text += text;
+
+                // Scroll to bottom
+                textBoxDebugger.SelectionStart = textBoxDebugger.Text.Length;
+                textBoxDebugger.ScrollToCaret();
+            }));
+        }
+
+        private void onCBClassChangeEvent(object sender, EventArgs e)
+        {
+            cbWeapon.Items.Clear();
+            switch (cbClass.Text)
+            {
+                case "Scout":
+                    foreach (var weapon in tf2ScoutWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 420;
+                    break;
+                case "Soldier":
+                    foreach (var weapon in tf2SoldierWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 460;
+                    break;
+                case "Pyro":
+                    foreach (var weapon in tf2PyroWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 735;
+                    break;
+                case "Demoman":
+                    foreach (var weapon in tf2DemomanWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 400;
+                    break;
+                case "Heavy":
+                    foreach (var weapon in tf2HeavyWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 645;
+                    break;
+                case "Engineer":
+                    foreach (var weapon in tf2EngineerWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 240;
+                    break;
+                case "Medic":
+                    foreach (var weapon in tf2MedicWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 340;
+                    break;
+                case "Sniper":
+                    foreach (var weapon in tf2SniperWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 315;
+                    break;
+                case "Spy":
+                    foreach (var weapon in tf2SpyWeapons)
+                        cbWeapon.Items.Add(weapon);
+                    cbWeapon.DropDownWidth = 395;
+                    break;
+            }
+        }
+
+        private void onCBCrosshairChangeEvent(object sender, EventArgs e)
+        {
+            pictureBoxCrosshair.ImageLocation = PATH_TF2WSC_RESOURCES_PREVIEWS + cbCrosshair.Text + ".png";
+        }
+
+        private void generateCrosshairs()
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                textBoxTF2Path.Enabled = false;
+                btnReload.Enabled = false;
+                cbClass.Enabled = false;
+                cbCrosshair.Enabled = false;
+                cbWeapon.Enabled = false;
+            }));
+
+            writeToDebugger("Preparing vtf2tga process... ");
+            Process vtf2tgaProcess = new Process();
+            vtf2tgaProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            vtf2tgaProcess.StartInfo.FileName = textBoxTF2Path.Text + @"\bin\vtf2tga.exe";
+            writeLineToDebugger("Done!");
+
+            writeToDebugger("Running vtf2tga.exe... ");
+            // Generate previews
+            foreach (string vtfFile in Directory.GetFiles(PATH_TF2WSC_RESOURCES_MATERIALS, "*.vtf"))
+            {
+                //writeLineToDebugger("vtf2tga.exe -i " + Path.GetFileName(vtfFile));
+                vtf2tgaProcess.StartInfo.Arguments = @"/C -i " + vtfFile;
+                vtf2tgaProcess.Start();
+            }
+            vtf2tgaProcess.WaitForExit();
+            writeLineToDebugger("Done!");
+
+            moveFilesByExtensionOrDelete(PATH_TF2WSC_RESOURCES_MATERIALS, PATH_TF2WSC_RESOURCES_PREVIEWS, "tga");
+
+            writeToDebugger("Generating generatepreviews.bat... ");
+            // Create or clear generatepreviews.bat
+            File.Create(FILEPATH_GENERATEPREVIEWSBAT).Close();
+
+            using (StreamWriter sw = new StreamWriter(FILEPATH_GENERATEPREVIEWSBAT))
+            {
+                foreach (string tgaFile in Directory.GetFiles(PATH_TF2WSC_RESOURCES_PREVIEWS, "*.tga"))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(tgaFile);
+                    sw.WriteLine(PATH_TF2WSC_RESOURCES + "ffmpeg.exe -i " + tgaFile + " " + filename + ".png");
+                }
+                sw.WriteLine("exit");
+            }
+            writeLineToDebugger("Done!");
+
+            writeToDebugger("Preparing generatepreviews process... ");
+            Process generatepreviewsProcess = new Process();
+            generatepreviewsProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            generatepreviewsProcess.StartInfo.FileName = FILEPATH_GENERATEPREVIEWSBAT;
+            writeLineToDebugger("Done!");
+
+            writeToDebugger("Running generatepreviews.bat... ");
+            generatepreviewsProcess.Start();
+            generatepreviewsProcess.WaitForExit();
+            writeLineToDebugger("Done!");
+
+            moveFilesByExtensionOrDelete(PATH_TF2WSC, PATH_TF2WSC_RESOURCES_PREVIEWS, "png");
+
+            // Add to ComboBox
+            Invoke(new MethodInvoker(delegate ()
+            {
+                cbCrosshair.Items.Clear();
+                foreach (var crosshair in Directory.GetFiles(PATH_TF2WSC_RESOURCES_PREVIEWS, "*.png"))
+                {
+                    string crosshairName = Path.GetFileNameWithoutExtension(crosshair);
+                    cbCrosshair.Items.Add(crosshairName);
+                }
+            }));
+
+            Invoke(new MethodInvoker(delegate ()
+            {
+                textBoxTF2Path.Enabled = true;
+                btnReload.Enabled = true;
+                cbClass.Enabled = true;
+                cbCrosshair.Enabled = true;
+                cbWeapon.Enabled = true;
+            }));
+
+            writeLineToDebugger("Finished reloading crosshair list!");
+        }
+
+        private void moveFilesByExtensionOrDelete(string sourceDirectory, string targetDirectory, string extension)
+        {
+            foreach (string file in Directory.GetFiles(sourceDirectory, "*." + extension))
+                if (!File.Exists(targetDirectory + Path.GetFileName(file)))
+                    File.Move(file, targetDirectory + Path.GetFileName(file));
+                else
+                    File.Delete(file);
+        }
+
+        private bool performSanityCheck()
+        {
+            string path = textBoxTF2Path.Text;
+
+            // Check if specified directory exist
+            writeToDebugger("Does given path exist? ");
+            if (!Directory.Exists(path))
+            {
+                writeLineToDebugger("No!");
+                MessageBox.Show("The specified TF2 path does not seem to exist.\nDid you set the correct path?", "TF2 Path does not exist", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            writeLineToDebugger("Yes.");
+
+            // Check if specified TF2 Path contains hl2.exe
+            writeToDebugger("Does \"hl2.exe\" exist? ");
+            if (!File.Exists(path + @"\hl2.exe"))
+            {
+                writeLineToDebugger("No!");
+                MessageBox.Show("The specified TF2 path does not contain \"hl2.exe\".\nDid you set the correct path?", "TF2 Path invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            writeLineToDebugger("Yes.");
+
+            // Check if vtf2tga.exe exists
+            writeToDebugger("Does \"vtf2tga.exe\" exist? ");
+            if (!File.Exists(path + @"\bin\vtf2tga.exe"))
+            {
+                writeLineToDebugger("No!");
+                MessageBox.Show("Could not find \"vta2tga.exe\".\nPlease verify game files.", "vta2tga.exe does not exist", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            writeLineToDebugger("Yes.");
+
+            // Check if project contains ffmpeg.exe
+            writeToDebugger("Does \"ffmpeg.exe\" exist? ");
+            if (!File.Exists(PATH_TF2WSC + @"\resources\ffmpeg.exe"))
+            {
+                writeLineToDebugger("No!");
+                MessageBox.Show("Could not find \"ffmpeg.exe\".\nPlease download the latest release of TF2WSC.\n(If that doesn't work create GitHub issue.)", "ffmpeg.exe missing from project", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            writeLineToDebugger("Yes.");
+
+            return true;
+        }
+    }
+}
