79f902f592896c1f0fc50d6a5e770a60d3948ce6
hbivnm <47778018+hbivnm@users.noreply.github.com>
2024-09-02 14:08:18 +0200

    Make window sizable, limit entries and add partial paths for VPK/script warning

--- VenomCrosshairs/FormMain.Designer.cs
+++ VenomCrosshairs/FormMain.Designer.cs
@@ -598,7 +598,6 @@
             this.Controls.Add(this.cbClass);
             this.Controls.Add(this.textBoxTF2Path);
             this.Controls.Add(this.lblTF2Path);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormMain";
